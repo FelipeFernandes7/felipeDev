287ad5b60452c86e269e29b2d63b036f22f0ebcd
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-07-28 16:21:31 -0300

    message:first commit

---
+++ src/components/header/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.scss";
+export function Header() {
+  return (
+    <div className="container">
+      <div className="logo">
+        <h1>logo</h1>
+      </div>
+      <nav>
+        <h1>teste</h1>
+      </nav>
+    </div>
+  );
+}
