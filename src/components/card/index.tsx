e48bc8adc665f93b45e64d495c47d4744cb8138a
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-11-07 12:06:26 -0300

    create a card

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,31 +1,69 @@
-import { Container } from "../container";
-import profileImg from "../../assets/picture-profile.jpg";
-import {
-  InformationCard,
-  Description,
-  ProfileImage,
-  Section,
-  Title,
-  SubTitle,
-  SmallTitle,
-} from "./styles";
-import { pages } from "../../locale/pt/pages";
-import { Social } from "../social";
+import * as Chakra from "@chakra-ui/react";
+import { IconType } from "react-icons";
 
-export function Card() {
+interface CardProps {
+  icon: IconType;
+  label: string;
+}
+export function Card({ label, icon }: CardProps) {
   return (
-    <Container>
-      <Section>
-        <ProfileImage src={profileImg} />
-        <InformationCard>
-          <SubTitle>{pages.card.title}</SubTitle>
-          <Title>{pages.card.name}</Title>
-          <SmallTitle>{pages.card.profession}</SmallTitle>
-          <Description>{pages.card.description}</Description>
-          <h1>Minhas redes sociais</h1>
-          <Social/>
-        </InformationCard>
-      </Section>
-    </Container>
+    <Chakra.Flex
+      w={"full"}
+      h={{ md: "200px", base: "150px" }}
+      maxW={{ md: "200px", base: "150px" }}
+      flexDirection={"column"}
+      borderRadius={"lg"}
+      cursor={"pointer"}
+      _hover={{
+        transform: "scale(1.1)",
+        transition: "all 0.3s ease",
+      }}
+      sx={{
+        filter: "drop-shadow(0 0 5px rgba(8, 145, 178, 0.8))",
+        animation: "glow 2.5s ease-in-out infinite",
+        "@keyframes glow": {
+          "0%": {
+            filter: "drop-shadow(0 0 5px rgba(8, 145, 178, 0.8))",
+          },
+          "50%": {
+            filter: "drop-shadow(0 0 15px rgba(8, 145, 178, 1))",
+          },
+          "100%": {
+            filter: "drop-shadow(0 0 5px rgba(8, 145, 178, 0.8))",
+          },
+        },
+      }}
+    >
+      <Chakra.Box
+        w={"full"}
+        h={"full"}
+        display={"flex"}
+        alignItems={"center"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        borderRadius={"lg"}
+      >
+        <Chakra.Icon as={icon} fontSize={"3rem"} color={"#fff"} />
+        <Chakra.Text
+          mt={"1rem"}
+          fontSize={"1rem"}
+          textAlign={"center"}
+          color={"#fff"}
+          sx={{
+            textShadow: `
+      0 0 5px #fff, 
+      0 0 5px #fff, 
+      0 0 5px rgb(8 145 178), 
+      0 0 5px rgb(8 145 178), 
+      0 0 5px rgb(8 145 178), 
+      0 0 5px rgb(8 145 178), 
+      0 0 8px rgb(8 145 178)
+    `,
+          }}
+        >
+          {label}
+        </Chakra.Text>
+      </Chakra.Box>
+    </Chakra.Flex>
   );
 }
